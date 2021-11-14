dbe5b3fb1f3f0c7266d4e016f59a22f975a742e9
Masood Abdolian <masood.abdolian@connect2wow.com>
2021-11-14 11:41:09 +0330

    Squashed 'packages/docs/' content from commit 9717e7a
    git-subtree-dir: packages/docs
    git-subtree-split: 9717e7a620d6ad35b419b4274ecbf911628bcf36

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Contributors from '$lib/Contributors.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<main>
+	<Contributors contributors={['abdolian']}/>
+	<ul>
+		<li class:active={$page.path === '/'}>
+			<a sveltekit:prefetch href="/">Home</a>
+		</li>
+	</ul>
+</main>

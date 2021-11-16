3efb22c8745f9cdb76cb574c1a77ec85f994b955
Masood Abdolian <masood.abdolian@connect2wow.com>
2021-11-16 22:57:11 +0330

    add new docs

---
+++ packages/docs.new/src/pages/_document.tsx
@@ -0,0 +1,51 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+// import * as Constants from '@app/constants';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-180789652-1"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'UA-180789652-1');
+              `,
+            }}
+          />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+
+        {/* TODO: global config */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.htmlplus = {
+                components: {
+                  divider: {
+                    properties: {
+                      size: 'sm'
+                    }
+                  }
+                }
+              }
+            `,
+          }}
+        />
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

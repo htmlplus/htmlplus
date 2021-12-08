7a17c693731a1cf64e864a8e28b7fa266eb1dfd7
Masood Abdolian <masood.abdolian@connect2wow.com>
2021-12-08 10:02:26 +0330

    update

--- packages/document/svelte.config.js
+++ packages/document/svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -10,10 +8,6 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		paths: {
-			base: dev ? '' : '/document'
-		},
-		// appDir: 'internal',
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',

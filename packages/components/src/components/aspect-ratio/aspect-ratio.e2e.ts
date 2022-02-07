b5723f161537b62a5519d0166f968e4be2433db0
Masood Abdolian <masood.abdolian@connect2wow.com>
2022-02-07 14:42:19 +0330

    clean

--- packages/components/src/components/aspect-ratio/aspect-ratio.e2e.ts
+++ packages/components/src/components/aspect-ratio/aspect-ratio.e2e.ts
@@ -1,41 +1,49 @@
-let $element;
+let element;
+
+const values = [
+  [1 / 2, 0.5],
+  [2 / 1, 2.0],
+  ['1/2', 0.5],
+  ['2/1', 2.0],
+  ['2/2', 1.0],
+  ['9/2', 4.5],
+];
 
 describe('plus-aspect-ratio', () => {
   beforeEach(() => {
     cy.setContent(`<plus-aspect-ratio><div style="background: #ddd">Content</div></plus-aspect-ratio>`);
-    cy.get('plus-aspect-ratio').then((element) => $element = element);
+    cy.get('plus-aspect-ratio').then((el) => element = el);
   });
 
   it('default value', () => {
-    expect($element.width()).to.eq($element.height());
+    expect(element.width()).to.eq(element.height());
   });
 
   it('child size', async () => {
-
-    await $element[0].setAttribute('value', 5);
-
-    const $child = $element.find('*');
-
-    expect($element.width()).to.eq($child.width());
-
-    expect($element.height()).to.eq($child.height());
+    await element[0].setAttribute('value', 5);
+    const $child = element.find('*');
+    expect(element.width()).to.eq($child.width());
+    expect(element.height()).to.eq($child.height());
   });
 
   it('value attribute', () => {
-    const cases = [
-      [1 / 2, 0.5],
-      [2 / 1, 2.0],
-      ['1/2', 0.5],
-      ['2/1', 2.0],
-      ['2/2', 1.0],
-      ['9/2', 4.5],
-    ];
-    cy.wrap(cases).each(async ([input, output]) => {
-      await $element[0].setAttribute('value', input);
-
-      const ratio = parseFloat(($element.width() / $element.height()).toFixed(2));
-
+    cy.wrap(values).each(async (value) => {
+      const [input, output] = value;
+      await element[0].setAttribute('value', input);
+      const ratio = parseFloat((element.width() / element.height()).toFixed(2));
       expect(ratio).to.eq(output);
     });
+  });
+
+  it.skip('attribute to property', () => {
+    cy.wrap(values).each(async (value) => {
+      const [input] = value;
+      await element[0].setAttribute('value', input);
+      expect(element[0]).to.have.property('value').and.eq(input.toString());
+    });
+  });
+
+  it.skip('property to attribute', () => {
+    cy.wrap(values).each(async (value) => { });
   });
 });

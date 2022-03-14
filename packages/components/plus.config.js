c43016b5b07ac2319542358a6f3f3c04d3065c75
Masood Abdolian <m.abdolian@gmail.com>
2022-03-15 00:36:45 +0330

    update

--- packages/components/plus.config.js
+++ packages/components/plus.config.js
@@ -8,28 +8,30 @@ import {
   sass,
   uhtml,
   validate,
-} from '@htmlplus/element/compiler';
+} from "@htmlplus/element/compiler";
 
 export default [
   read(),
   parse(),
   validate(),
   extract({
-    prefix: 'plus',
+    prefix: "plus",
   }),
   sass({
-    loadPaths: ['./src/styles'],
+    loadPaths: ["./src/styles"],
   }),
   attach(),
   uhtml(),
   print(),
   reactProxy({
     categorize: true,
-    corePackageName: '@htmlplus/components',
-    dist: '../ports/react.new',
-    importerComponent:
-      '@htmlplus/components/dist/components/{{fileName}}/{{fileName}}#{{componentClassName}}',
-    importerComponentType:
-      '@htmlplus/components/dist/components/{{fileName}}/{{fileName}}#{{componentClassName}}JSX',
+    corePackageName: "@htmlplus/components",
+    dist: "../ports/react.new",
+    importerComponent(context) {
+      return `@htmlplus/components/dist/components/${context.fileName}/${context.fileName}#${context.componentClassName}`;
+    },
+    importerComponentType(context) {
+      return `@htmlplus/components/dist/components/${context.fileName}/${context.fileName}#${context.componentClassName}JSX`;
+    },
   }),
 ];

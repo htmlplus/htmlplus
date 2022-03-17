768c4d9de36af7fa3532d4b83161f61528aae671
Masood Abdolian <m.abdolian@gmail.com>
2022-03-17 23:06:40 +0330

    clean

--- packages/proxies/react/src/components/dialog.compact.ts
+++ packages/proxies/react/src/components/dialog.compact.ts
@@ -9,15 +9,15 @@ import { Dialog } from './dialog';
 import { DialogBody } from './dialog-body';
 import { DialogHeader } from './dialog-header';
 import { DialogFooter } from './dialog-footer';
-import { DialogToggler } from './dialog-toggler';
 import { DialogContent } from './dialog-content';
+import { DialogToggler } from './dialog-toggler';
 
 const All = /*@__PURE__*/ Object.assign(Dialog, {
   Body: DialogBody,
   Header: DialogHeader,
   Footer: DialogFooter,
-  Toggler: DialogToggler,
   Content: DialogContent,
+  Toggler: DialogToggler,
 });
 
 export { All as Dialog }
--- packages/proxies/react/src/components/index.ts
+++ packages/proxies/react/src/components/index.ts
@@ -3,10 +3,9 @@
  **************************************************/
 
 export * from './aspect-ratio';
-export * from './bottom-navigation.compact';
 export * from './breadcrumb';
 export * from './browse';
-export * from './card.compact';
+export * from './card';
 export * from './click-outside';
 export * from './cropper';
 export * from './dialog.compact';
@@ -15,15 +14,11 @@ export * from './drawer.compact';
 export * from './grid.compact';
 export * from './icon';
 export * from './intersection';
-export * from './menu';
 export * from './portal';
-export * from './ripple';
 export * from './scroll-indicator';
 export * from './spinner';
 export * from './sticky';
 export * from './switch';
 export * from './tabs.compact';
-export * from './toast.compact';
-export * from './toolbar.compact';
 export * from './tooltip';
 export * from './transition';

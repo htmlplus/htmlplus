f1eef317eb812ad79a0a39ab34b3ad4fa5cc63f6
Masood Abdolian <m.abdolian@gmail.com>
2022-04-30 03:06:08 +0430

    update contexts type

--- packages/core/plus.config.js
+++ packages/core/plus.config.js
@@ -5,38 +5,37 @@ import {
   print,
   reactProxy,
   read,
-  sass,
+  scss,
+  style,
   uhtml,
-  validate,
-} from "@htmlplus/element/compiler";
+  validate
+} from '@htmlplus/element/compiler';
 
 export default [
   read(),
   parse(),
   validate(),
   extract({
-    prefix: "plus",
+    prefix: 'plus'
   }),
-  sass({
-    loadPaths: ["./src/styles"],
+  style(),
+  scss({
+    loadPaths: ['./src/styles']
   }),
   attach(),
   uhtml(),
   print(),
   reactProxy({
     compact: true,
-    dist: "../proxies/react",
+    dist: '../proxies/react',
     eventName(eventName) {
-      return eventName.replace(
-        /plus(\S*)/g,
-        (match, group) => group.charAt(0).toLowerCase() + group.substr(1)
-      );
+      return eventName.replace(/plus(\S*)/g, (match, group) => group.charAt(0).toLowerCase() + group.substr(1));
     },
     importerComponent(context) {
       return `@htmlplus/core/dist/components/${context.fileName}/${context.fileName}#${context.componentClassName}`;
     },
     importerComponentType(context) {
       return `@htmlplus/core/dist/components/${context.fileName}/${context.fileName}#${context.componentClassName}JSX`;
-    },
-  }),
+    }
+  })
 ];

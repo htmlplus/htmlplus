8f93ec49bcc0ec620e634e9d12eec7f08ce236cf
Masood Abdolian <m.abdolian@gmail.com>
2022-05-17 00:35:44 +0430

    update e2e test

--- packages/core/src/components/click-outside/click-outside.e2e.ts
+++ packages/core/src/components/click-outside/click-outside.e2e.ts
@@ -1,27 +1,32 @@
 /// <reference types="cypress" />
 
 describe('plus-click-outside', () => {
-  let element: Cypress.Chainable<JQuery<HTMLElement>> = null;
-
   beforeEach(() => {
     cy.setContent(`<plus-click-outside></plus-click-outside>`);
-    element = cy.get('plus-click-outside');
   });
 
   it('display should be block', () => {
-    element.should('have.css', 'display', 'block');
+    cy.get('@element').should('have.css', 'display', 'block');
   });
 
   it('height should be 0px', () => {
-    element.should('have.css', 'height', '0px');
+    cy.get('@element').should('have.css', 'height', '0px');
   });
 
   it('width should not be 0px', () => {
-    element.should('not.have.css', 'width', '0px');
+    cy.get('@element').should('not.have.css', 'width', '0px');
   });
 
-  it('width should be match with parent', () => {
-    element.parent().invoke('attr', 'style', 'width: 200px');
-    element.should('have.css', 'width', '200px');
+  it('width should be match with own parent', () => {
+    cy.get('@parent').invoke('attr', 'style', 'width: 200px');
+    cy.get('@element').should('have.css', 'width', '200px');
   });
+
+  it('element should not have a disabled attribute', () => {
+    cy.get('@element').should('not.have.attr', 'disabled');
+  });
+
+  it('disabled property should be inactive in the initial state', () => {});
+
+  it('disabled property should be reflected on the attribute', () => {});
 });

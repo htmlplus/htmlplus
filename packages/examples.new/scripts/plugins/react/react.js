f6d881c51bd717cf0966ff21db550a630c2e81f6
Masood Abdolian <masood.abdolian@connect2wow.com>
2022-05-29 12:56:53 +0430

    use experimental render template

--- packages/examples.new/scripts/plugins/react/react.js
+++ packages/examples.new/scripts/plugins/react/react.js
@@ -1,6 +1,12 @@
+import { __dirname, renderTemplate } from '@htmlplus/element/compiler/utils/index.js';
+
 export const react = () => {
   const name = "react";
-  const next = (context) => {};
+  const next = (context) => {
+    const config = { cwd: __dirname(import.meta.url) };
+    const patterns = ['templates/**/*.*'];
+    renderTemplate(patterns, 'src/aspect-ratio/default/react', config)({});
+  };
   return {
     name,
     next,
---
+++ packages/examples.new/src/aspect-ratio/default/readme.md
@@ -0,0 +1,14 @@
+```css [style]
+plus-card {
+  --plus-card-background-color: #c5cae9;
+  padding: 1rem;
+}
+```
+
+```html [template]
+<plus-aspect-ratio value="16/9">
+  <plus-card tile>
+    This box will always be 16/9 (unless you put more stuff in it)
+  </plus-card>
+</plus-aspect-ratio>
+```

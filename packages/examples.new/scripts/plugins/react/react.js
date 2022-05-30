bb10ee12533586200e5a0aa3adbb44800081c869
Masood Abdolian <m.abdolian@gmail.com>
2022-05-31 00:18:18 +0430

    update

--- packages/examples.new/scripts/plugins/react/react.js
+++ packages/examples.new/scripts/plugins/react/react.js
@@ -1,63 +1,198 @@
-import { __dirname, print, renderTemplate, visitor } from '@htmlplus/element/compiler/utils/index.js';
-import t from '@babel/types';
-import template from '@babel/template';
-import { pascalCase } from 'change-case';
+import {
+  __dirname,
+  print,
+  renderTemplate,
+  visitor,
+} from "@htmlplus/element/compiler/utils/index.js";
+import t from "@babel/types";
+import template from "@babel/template";
+import { camelCase, pascalCase, paramCase } from "change-case";
 
-import path from 'path';
+import path from "path";
 
 export const react = () => {
   const name = "react";
   const next = (context) => {
-
-    const state = {
-      react: new Set(),
-      components: new Set()
-    };
+    const dependencies = new Set();
 
     const script = {
       AssignmentExpression(path) {
         const { left, right } = path.node;
-        if (left && left.object && left.object.type === 'ThisExpression') {
-          const setter = 'set' + pascalCase(left.property.name);
+        if (left && left.object && left.object.type === "ThisExpression") {
+          const setter = "set" + pascalCase(left.property.name);
           path.replaceWith(template.expression.ast`${setter}(${right})`);
         }
       },
       ClassDeclaration(path) {
-        path.replaceWithMultiple(
-          [
-            t.variableDeclaration(
-              'const',
-              [
-                t.variableDeclarator(
-                  t.identifier('App'),
-                  t.arrowFunctionExpression(
-                    [],
-                    t.blockStatement([])
-                  )
-                )
-              ]
+        path.traverse(script);
+
+        const body = [
+          t.importDeclaration(
+            [
+              t.importDefaultSpecifier(t.identifier("React")),
+              ...Array.from(dependencies.keys()).map((key) =>
+                t.importSpecifier(t.identifier(key), t.identifier(key))
+              ),
+            ],
+            t.stringLiteral("react")
+          ),
+          t.variableDeclaration("const", [
+            t.variableDeclarator(
+              t.identifier("App"),
+              t.arrowFunctionExpression(
+                [],
+                t.blockStatement(path.node.body.body)
+              )
             ),
-            t.exportDefaultDeclaration(
-              t.identifier('App')
-            )
-          ]
-        );
+          ]),
+          t.exportDefaultDeclaration(t.identifier("App")),
+        ];
+
+        path.replaceWithMultiple(body);
+
+        dependencies.clear();
       },
-    }
+      ClassMethod(path) {
+        const { body, key, params } = path.node;
 
-    visitor(context.fileAST, script)
+        if (key.name !== "render") {
+          path.replaceWith(
+            t.variableDeclaration("const", [
+              t.variableDeclarator(
+                key,
+                t.arrowFunctionExpression(params, body)
+              ),
+            ])
+          );
+          return;
+        }
 
-    const source = 'templates/**/*.*'
+        const statement = body.body.find(
+          (element) => element.type === "ReturnStatement"
+        );
 
-    const destination = path.join(context.directoryPath, 'react')
+        if (
+          statement &&
+          statement.argument &&
+          statement.argument.type === "JSXElement"
+        ) {
+          let element = statement.argument;
+
+          let children = [t.jsxText("\n"), element, t.jsxText("\n")];
+
+          if (element.openingElement.name.name.match(/fragment/)) {
+            children = element.children;
+            element = element.children.find(
+              (element) => element.type === "JSXElement"
+            );
+          }
+
+          statement.argument = t.jsxElement(
+            t.jsxOpeningElement(t.jsxIdentifier(""), []),
+            t.jSXClosingElement(t.jsxIdentifier("")),
+            children
+          );
+        }
+
+        path.replaceWithMultiple(body.body);
+      },
+      ClassProperty(path) {
+        const { decorators, key, value } = path.node;
+
+        if (
+          decorators &&
+          decorators[0] &&
+          decorators[0].expression.callee.name === "State"
+        ) {
+          const setter = "set" + pascalCase(key.name);
+
+          path.replaceWith(
+            t.variableDeclaration("let", [t.variableDeclarator(key, value)])
+          );
+
+          dependencies.add("useState");
+
+          path.replaceWith(
+            t.variableDeclaration("const", [
+              t.variableDeclarator(
+                t.arrayPattern([key, t.identifier(setter)]),
+                t.callExpression(t.identifier("useState"), value ? [value] : [])
+              ),
+            ])
+          );
+        } else {
+          path.replaceWith(
+            t.variableDeclaration("let", [t.variableDeclarator(key, value)])
+          );
+        }
+      },
+      MemberExpression(path) {
+        const { property, object } = path.node;
+        if (object.type === "ThisExpression") path.replaceWith(property);
+      },
+      JSXAttribute(path) {
+        const { name } = path.node;
+
+        // TODO
+        name.name = name.name.replace("onPlus", "on");
+
+        name.name = camelCase(name.name);
+
+        // TODO
+        if (/data[A-Z]/g.test(name.name)) name.name = paramCase(name.name);
+
+        if (name.name === "class") name.name = "className";
+      },
+      JSXElement(path) {},
+      JSXText(path) {
+        const { value } = path.node;
+
+        let level = 1;
+
+        let parent = path.parentPath;
+
+        while (parent.type !== "ReturnStatement") {
+          level++;
+          parent = parent.parentPath;
+        }
+
+        let space = "";
+
+        for (let i = 0; i < level; i++) space += "  ";
+
+        const trim = value.trim();
+
+        const from = value.indexOf(trim);
+
+        const to = trim.length;
+
+        const startIndex = value.indexOf("\n");
+
+        const endIndex = value.lastIndexOf("\n");
+
+        const start = startIndex !== -1 && from > startIndex;
+
+        const end = endIndex !== -1 && to <= endIndex;
+
+        path.node.value = `${start ? "\n" : ""}${space}${value.trim()}${
+          end ? "\n" : ""
+        }`;
+      },
+    };
+
+    visitor(context.fileAST, script);
+
+    const source = "templates/**/*.*";
+
+    const destination = path.join(context.directoryPath, "react");
 
     const config = {
-      cwd: __dirname(import.meta.url)
-    }
+      cwd: __dirname(import.meta.url),
+    };
 
     const model = {
-      script: print(context.fileAST)
-    }
+      script: print(context.fileAST),
+    };
 
     renderTemplate(source, destination, config)(model);
   };

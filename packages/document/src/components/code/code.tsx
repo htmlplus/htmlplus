9280110adfd0df38e2f15eec145a9f237bb12bf9
Masood Abdolian <masood.abdolian@connect2wow.com>
2022-06-16 12:17:39 +0430

    add prismjs

--- packages/document/src/components/code/code.tsx
+++ packages/document/src/components/code/code.tsx
@@ -1,5 +1,19 @@
+import React, { useEffect, useMemo, useRef } from 'react';
+
+import Prism from 'prismjs';
+import 'prismjs/themes/prism-tomorrow.css';
+
 import { CodeProps } from './code.types';
 
-export const Code = ({ children, copy = true, flat, language }: CodeProps) => {
-  return null;
+export const Code = ({ children, language }: CodeProps) => {
+  const element = useRef(null);
+  const content = useMemo(() => children?.toString() || '', [children]);
+  useEffect(() => Prism.highlightAllUnder(element.current!), [content, language]);
+  return (
+    <div ref={element}>
+      <pre className={`language-${language}`} tabIndex={0}>
+        <code className={`language-${language}`}>{content}</code>
+      </pre>
+    </div>
+  )
 };

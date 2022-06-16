06a7b2ea068453163073faafa827053b141e2b71
Masood Abdolian <m.abdolian@gmail.com>
2022-06-16 20:04:36 +0430

    clean

--- packages/document/src/data/sidebar.ts
+++ packages/document/src/data/sidebar.ts
@@ -7,53 +7,17 @@ export const sidebar = [
     icon: 'introduction',
     items: [
       {
-        title: `What’s ${Constants.PLATFORM_NAME}?`,
+        title: `What's ${Constants.PLATFORM_NAME}?`,
         route: {
-          to: 'ROUTE:INTRODUCTION:WHAT'
+          to: Constants.ROUTE.INTRODUCTION_WHAT
         }
       },
       {
         title: `Why ${Constants.PLATFORM_NAME}?`,
         route: {
-          to: 'ROUTE:INTRODUCTION:WHY'
+          to: Constants.ROUTE.INTRODUCTION_WHY
         }
       }
-      // {
-      //     title: 'Long-term Support',
-      //     route: {
-      //         to: 'ROUTE:INTRODUCTION:QUICKSTART',
-      //     },
-      // },
-      // {
-      //     title: 'Vision',
-      //     route: {
-      //         to: 'ROUTE:INTRODUCTION:VISION',
-      //     },
-      // },
-      // {
-      //     title: 'Roadmap',
-      //     route: {
-      //         to: 'ROUTE:INTRODUCTION:QUICKSTART',
-      //     },
-      // },
-      // {
-      //     title: 'Versions',
-      //     route: {
-      //         to: 'ROUTE:INTRODUCTION:QUICKSTART',
-      //     },
-      // },
-      // {
-      //     title: 'Release notes',
-      //     route: {
-      //         to: 'ROUTE:INTRODUCTION:QUICKSTART',
-      //     },
-      // },
-      // {
-      //     title: 'Licence',
-      //     route: {
-      //         to: 'ROUTE:INTRODUCTION:QUICKSTART',
-      //     },
-      // },
     ]
   },
   {
@@ -63,114 +27,24 @@ export const sidebar = [
       {
         title: 'Installation',
         route: {
-          to: 'ROUTE:INSTALLATION'
+          to: Constants.ROUTE.INSTALLATION
         }
       },
-      // {
-      //     title: 'Frequently asked questions',
-      //     route: {
-      //         to: 'ROUTE:INTRODUCTION:QUICKSTART',
-      //     },
-      // },
       {
         title: 'Browser support',
         route: {
-          to: 'ROUTE:GETTINGSTARTED:BROWSERSUPPORT'
+          to: Constants.ROUTE.GETTINGSTARTED_BROWSERSUPPORT
         }
       }
-      // {
-      //     title: 'Upgrade guide',
-      //     route: {
-      //         to: 'ROUTE:INTRODUCTION:QUICKSTART',
-      //     },
-      // },
-      // {
-      //     title: 'Contributing',
-      //     route: {
-      //         to: 'ROUTE:INTRODUCTION:QUICKSTART',
-      //     },
-      // },
     ]
   },
-  // {
-  //     title: 'Features',
-  //     icon: 'features',
-  //     items: [
-  //         {
-  //             title: 'Global config',
-  //             route: {
-  //                 to: 'ROUTE:INTRODUCTION:QUICKSTART',
-  //             },
-  //         },
-  //         {
-  //             title: 'Display Breakpoint',
-  //             route: {
-  //                 to: 'ROUTE:INTRODUCTION:QUICKSTART',
-  //             },
-  //         },
-  //         {
-  //             title: 'Framework Integrations',
-  //             route: {
-  //                 to: 'ROUTE:INTRODUCTION:QUICKSTART',
-  //             },
-  //         },
-  //         {
-  //             title: 'Accessibility (a11y)',
-  //             route: {
-  //                 to: 'ROUTE:INTRODUCTION:QUICKSTART',
-  //             },
-  //         },
-  //         {
-  //             title: 'Bidirectionality (LTR/RTL)',
-  //             route: {
-  //                 to: 'ROUTE:INTRODUCTION:QUICKSTART',
-  //             },
-  //         },
-  //         {
-  //             title: 'CSS Reset',
-  //             route: {
-  //                 to: 'ROUTE:INTRODUCTION:QUICKSTART',
-  //             },
-  //         },
-  //         {
-  //             title: 'Server side rendering',
-  //             route: {
-  //                 to: 'ROUTE:INTRODUCTION:QUICKSTART',
-  //             },
-  //         },
-  //         {
-  //             title: 'Cancelable event',
-  //             route: {
-  //                 to: 'ROUTE:INTRODUCTION:QUICKSTART',
-  //             },
-  //         },
-  //     ]
-  // },
-  // {
-  //     title: 'Animations',
-  //     icon: 'animations',
-  //     items: [
-  //         {
-  //             title: 'Animation Guideline',
-  //             route: {
-  //                 to: 'ROUTE:INTRODUCTION:QUICKSTART',
-  //             },
-  //         },
-  //         {
-  //             title: 'Transition',
-  //             route: {
-  //                 to: 'ROUTE:INTRODUCTION:QUICKSTART',
-  //             },
-  //         },
-  //     ]
-  // },
   {
     title: 'UI Components',
     icon: 'components',
     items: components.map((component) => ({
       title: component.title,
       route: {
-        to: 'ROUTE:COMPONENT:DETAILS',
+        to: Constants.ROUTE.COMPONENT_DETAILS,
         params: {
           key: component.key
         }
@@ -184,56 +58,9 @@ export const sidebar = [
       {
         title: 'Code Of Conduct',
         route: {
-          to: 'ROUTE:ABOUT:CODEOFCONDUCT'
+          to: Constants.ROUTE.ABOUT_CODEOFCONDUCT
         }
       }
-      // {
-      //   title: 'Meet the team',
-      //   route: {
-      //     to: 'ROUTE:INTRODUCTION:QUICKSTART',
-      //   },
-      // },
-      // {
-      //   title: 'Sponsors and backers',
-      //   route: {
-      //     to: 'ROUTE:INTRODUCTION:QUICKSTART',
-      //   },
-      // },
     ]
   }
-  // {
-  //     title: 'Framework Integrations',
-  //     items: [
-  //         {
-  //             title: 'Overview',
-  //             route: {
-  //                 to: 'ROUTE:FRAMEWORK:OVERVIEW',
-  //             },
-  //         },
-  //         {
-  //             title: 'React',
-  //             route: {
-  //                 to: 'ROUTE:FRAMEWORK:REACT',
-  //             },
-  //         },
-  //         {
-  //             title: 'Angular',
-  //             route: {
-  //                 to: 'ROUTE:FRAMEWORK:ANGULAR',
-  //             },
-  //         },
-  //         {
-  //             title: 'Vue',
-  //             route: {
-  //                 to: 'ROUTE:FRAMEWORK:VUE',
-  //             },
-  //         },
-  //         {
-  //             title: 'JavaScript',
-  //             route: {
-  //                 to: 'ROUTE:FRAMEWORK:JAVASCRIPT',
-  //             },
-  //         },
-  //     ],
-  // },
 ];

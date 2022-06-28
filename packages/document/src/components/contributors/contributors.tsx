93f48cf4ea67a2b639044b3e50a54cdee488d8ec
Masood Abdolian <m.abdolian@gmail.com>
2022-06-28 20:16:50 +0430

    update

--- packages/document/src/components/contributors/contributors.tsx
+++ packages/document/src/components/contributors/contributors.tsx
@@ -5,27 +5,25 @@ import { ContributorsProps } from './contributors.types';
 export const Contributors = ({ contributors }: ContributorsProps) => {
   if (!contributors?.length) return null;
   return (
-    <section className="contributors">
-      <h5>
+    <>
+      <h2>
         <Toc.Item level={1}>Contributors</Toc.Item>
-      </h5>
-      <div>
-        <ul>
-          {contributors.reverse().map((contributor) => (
-            <li key={contributor}>
-              <a rel="noopener" href={`https://github.com/${contributor}`} target="_blank">
-                <img
-                  src={`https://github.com/${contributor}.png?size=90`}
-                  loading="lazy"
-                  width="32"
-                  height="32"
-                  title={`Contributor ${contributor}`}
-                />
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </section>
+      </h2>
+      <ul className="contributors">
+        {contributors.reverse().map((contributor) => (
+          <li key={contributor}>
+            <a rel="noopener" href={`https://github.com/${contributor}`} target="_blank">
+              <img
+                src={`https://github.com/${contributor}.png?size=90`}
+                loading="lazy"
+                width="32"
+                height="32"
+                title={`Contributor ${contributor}`}
+              />
+            </a>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };

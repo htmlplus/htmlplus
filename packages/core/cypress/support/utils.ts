fe23b0f9ae9719e9d6a0b4e667c807ea74ac5656
Masood Abdolian <m.abdolian@gmail.com>
2022-07-21 13:53:40 +0430

    update e2e utils

--- packages/core/cypress/support/utils.ts
+++ packages/core/cypress/support/utils.ts
@@ -1,7 +1,5 @@
 // TODO: problem with 'disabled' key
 // cy.get(selector).should('have.attr', key, `${init}`);
-
-// TODO
 // it(`"${key}" attribute should be updates "${key}" property`, () => {
 //   cy.get(selector).invoke('attr', key, true).invoke('prop', key).should('eq', true);
 // });
@@ -15,27 +13,26 @@
 export const property = (selector, key, type, init, reflect) => {
   const hasReflection = reflect && typeof init != 'undefined';
 
-  const values = [
-    /**
-     * For example
-     * [main, property, attribute]
-     */
-  ];
+  it(`"${key}" property should be exist`, () => {
+    cy.get(selector)
+      .then(($elements) => key in $elements[0])
+      .should('eq', true);
+  });
 
-  switch (type) {
-    case Boolean:
-      values.push([true, true, null]);
-      // values.push([false, false, false]);
-      break;
-    // TODO
-    // case Number:
-    //   values.push([1, 1, '1']);
-    //   break;
-    // case String:
-    //   values.push(['', '', '']);
-    //   values.push(['Test', 'Test', 'Test']);
-    //   break;
-  }
+  it(`"${key}" should has getter and setter`, () => {
+    const values = [undefined, 'undefined', null, 'null', false, 'false', true, 'true', -1, '-1', 0, '0', 1, '1'];
+    cy.wrap(values).each((value) => {
+      cy.get(selector)
+        .then(($elements) => {
+          $elements[0][key] = value;
+          return {
+            value: $elements[0][key]
+          };
+        })
+        .its('value')
+        .should('eq', value);
+    });
+  });
 
   it(`"${key}" property should be "${init}" in the initialize`, () => {
     cy.get(selector).invoke('prop', key).should('eq', init);
@@ -60,6 +57,23 @@ export const property = (selector, key, type, init, reflect) => {
 
   if (reflect) {
     it(`"${key}" property should be reflected on the attribute`, () => {
+      const values = (() => {
+        switch (type) {
+          case Boolean:
+            return [
+              [true, true, null],
+              [false, false, false]
+            ];
+          // TODO
+          // case Number:
+          //   return [[1, 1, '1']];
+          // case String:
+          //   return [
+          //     ['', '', ''],
+          //     ['Test', 'Test', 'Test']
+          //   ];
+        }
+      })();
       cy.wrap(values).each(([main, property, attribute]) => {
         cy.get(selector)
           .invoke('prop', key, main)

cacba03b8451c53eff14870224e44ca40e6dcbb0
Masood Abdolian <m.abdolian@gmail.com>
2022-07-23 15:58:36 +0430

    update index

--- packages/document/src/pages/index.tsx
+++ packages/document/src/pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from 'next';
 
+import { Transition } from '@app/components';
+
 const Home: NextPage = () => {
-  return <div>HTMLPLUS</div>;
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', height: '100vh', justifyContent: 'center' }}>
+      <Transition name="fade-in" repeat="infinite">
+        COMING SOON
+      </Transition>
+    </div>
+  );
 };
 
 export default Home;

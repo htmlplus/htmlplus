e6627b1b85dba17a415f54bc33ddac19f049f8f1
Masood Abdolian <m.abdolian@gmail.com>
2022-08-07 12:41:32 +0430

    update data

--- packages/document/src/data/statistics.ts
+++ packages/document/src/data/statistics.ts
@@ -1,9 +1,24 @@
+import { components, examples, frameworks } from '@app/data';
+
 export const statistics = {
-  components: 8,
-  dowanloads: 60,
-  examples: 32,
-  forks: 4,
-  frameworks: 3,
-  stars: 20,
+  dowanloads: 0,
+  forks: 0,
+  platforms: 0,
+  stars: 0,
   themes: 0,
+  get components() {
+    return this.componentsPerFramework * this.frameworks;
+  },
+  get componentsPerFramework() {
+    return components.length;
+  },
+  get examples() {
+    return this.examplesPerFramework * this.frameworks;
+  },
+  get examplesPerFramework() {
+    return examples.filter((example) => example.category == 'preview').length;
+  },
+  get frameworks() {
+    return frameworks.filter((framework) => !framework.disabled).length;
+  }
 };

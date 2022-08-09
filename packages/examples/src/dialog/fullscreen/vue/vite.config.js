11718a7e00068b7582bee0e1c2e457e277594089
Masood Abdolian <m.abdolian@gmail.com>
2022-08-10 00:12:46 +0430

    rebuild examples

--- packages/examples/src/dialog/fullscreen/vue/vite.config.js
+++ packages/examples/src/dialog/fullscreen/vue/vite.config.js
@@ -2,5 +2,9 @@ import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue({
+      isCustomElement: (tag) => tag.startsWith('plus-')
+    })
+  ]
 });

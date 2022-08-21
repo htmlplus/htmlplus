3981b6f2af3f709732389048fb2f1cda9ba19fdd
Masood Abdolian <m.abdolian@gmail.com>
2022-08-21 17:35:03 +0430

    build examples

--- packages/examples/src/dialog/persistent/vue/vite.config.js
+++ packages/examples/src/dialog/persistent/vue/vite.config.js
@@ -4,7 +4,11 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [
     vue({
-      isCustomElement: (tag) => tag.startsWith('plus-')
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.startsWith('plus-')
+        }
+      }
     })
   ]
 });

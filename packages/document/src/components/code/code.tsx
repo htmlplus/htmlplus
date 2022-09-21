001039ac91c475cf093f6dc309ce2b35a2c4cdc8
Masood Abdolian <m.abdolian@gmail.com>
2022-09-21 23:27:20 +0430

    add copy for code component

--- packages/document/src/components/code/code.tsx
+++ packages/document/src/components/code/code.tsx
@@ -2,16 +2,19 @@ import React, { useEffect, useMemo, useRef } from 'react';
 
 import Prism from 'prismjs';
 import 'prismjs/components/prism-jsx';
+import 'prismjs/plugins/toolbar/prism-toolbar';
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
+import 'prismjs/plugins/toolbar/prism-toolbar.css';
 import 'prism-themes/themes/prism-nord.css';
 
 import { CodeProps } from './code.types';
 
-export const Code = ({ children, language }: CodeProps) => {
+export const Code = ({ children, copy = true, language }: CodeProps) => {
   const element = useRef(null);
   const content = useMemo(() => children?.toString() || '', [children]);
   useEffect(() => Prism.highlightAllUnder(element.current!), [content, language]);
   return (
-    <pre ref={element} className={`language-${language}`} tabIndex={0}>
+    <pre ref={element} className={`language-${language}${copy ? ' copy' : ''}`} tabIndex={0}>
       <code className={`language-${language}`}>{content}</code>
     </pre>
   );
--- packages/document/src/components/code/code.types.tsx
+++ packages/document/src/components/code/code.types.tsx
@@ -2,9 +2,6 @@ import { ReactNode } from 'react';
 
 export interface CodeProps {
   children?: ReactNode;
-  language: 'html' | 'css' | 'js' | 'jsx' | 'shell';
-
-  // TODO
   copy?: boolean;
-  tile?: boolean;
+  language: 'html' | 'css' | 'js' | 'jsx' | 'shell';
 }

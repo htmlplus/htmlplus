7ecb92101fcd2d65dd05f538e55eb556393fdfcf
Masood Abdolian <m.abdolian@gmail.com>
2022-10-02 13:15:26 +0330

    update element package dev en

--- packages/element/src/dev/scripts/build.ts
+++ packages/element/src/dev/scripts/build.ts
@@ -1,12 +1,21 @@
 import compiler from '../../compiler/index.js';
-import { extract, parse, read, style, validate } from '../../compiler/index.js';
+import { customElement, extract, parse, read, style, validate } from '../../compiler/index.js';
 
-const { start, next, finish } = compiler(read(), parse(), validate(), extract(), style({}));
+const { start, next, finish } = compiler(
+  read(),
+  parse(),
+  validate(),
+  extract(),
+  style({}),
+  customElement({ typings: false })
+);
 
 (async () => {
   await start();
 
-  await next('./src/dev/components/element.tsx');
+  const result = await next('./src/dev/components/element.tsx');
+
+  console.log(result.script);
 
   await finish();
 })();

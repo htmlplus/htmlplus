583ebd6f9e5a086155390a35429ad9662af05c52
Masood Abdolian <masood.abdolian@connect2wow.com>
2022-10-03 16:16:24 +0330

    clean

--- packages/core/scripts/build.post.js
+++ packages/core/scripts/build.post.js
@@ -4,7 +4,11 @@ import fs from 'fs';
 cpy(['package-lock.json', 'README.md'], 'dist');
 
 const raw = fs.readFileSync('package.json', 'utf8');
+
 const parsed = JSON.parse(raw);
+
 delete parsed.scripts;
+
 const stringified = JSON.stringify(parsed, null, 2);
+
 fs.writeFileSync('dist/package.json', stringified);

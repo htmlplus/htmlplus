07962f2b47ee72cb804317c8da7af00488b4d6f1
Masood Abdolian <m.abdolian@gmail.com>
2022-10-20 00:21:55 +0330

    update

--- packages/element/src/dev/plus.config.ts
+++ packages/element/src/dev/plus.config.ts
@@ -1,3 +1,3 @@
-import { customElement, extract, parse, read, style, validate } from '../../dist/compiler/index.js';
+import { customElement, extract, parse, read, style, validate } from '../compiler/index.js';
 
 export default [read(), parse(), validate(), extract(), style(), customElement({ typings: false })];

cd3a70b6772d691f45a2d82d0a769cfd2f35f0bc
Masood Abdolian <m.abdolian@gmail.com>
2022-10-24 17:54:32 +0330

    update

--- packages/core/plus.config.js
+++ packages/core/plus.config.js
@@ -78,7 +78,7 @@ export default [
   webTypes({
     destination: 'dist/json/web-types.json',
     packageName: '@htmlplus/core',
-    packageVersion: '0.4.4',
+    packageVersion: '0.4.6',
     reference(context) {
       return `https://www.htmlplus.io/javascript/component/${context.componentKey}`;
     },
